734267756347b40f9b2ba6296733f4dcf73a5e1c
rafael reis <rafaeloliveira63@yahoo.com>
2022-06-11 17:27:41 -0300

    Feat: finishing the home page

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -2,7 +2,7 @@ import "./home.scss";
 
 import Header from "../../components/header/header";
 import CarouselApp from "../../components/header/carousel/carousel";
-
+import icone from "../../assets/icone.png";
 const Home = () => {
 
 
@@ -13,9 +13,26 @@ const Home = () => {
       <section>
         <Header />
         <div className="image">
+          <div>
+            <h1>Museu Universitário <span>Academia</span> </h1>
+            <img draggable="false" src={icone} alt="icone"></img>
+            
 
-        <h1>Home</h1>
+          </div>
+          <h1 className="title">Museu de Etnologia Indígena e História Natural  </h1> 
+     
         </div>
+        <main>
+          <div>
+            <div className="container">
+            <h1>Algumas de nossas Exposições</h1>
+
+            <CarouselApp/>
+            </div>
+
+          </div>
+         
+        </main>
         {/* <CarouselApp/> */}
       </section>
     </div>
